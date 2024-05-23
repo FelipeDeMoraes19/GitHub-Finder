e21e8b75e95528c217240835a4f3ce86ed6f444d
FelipeMoraes18 <111572342+FelipeMoraes18@users.noreply.github.com>
2024-05-23 07:25:43 -0300

    Router configuration done

--- github-finder/src/App.tsx
+++ github-finder/src/App.tsx
@@ -1,10 +1,13 @@
+import { Outlet } from "react-router"
+
 function App() {
 
   return (
     <div className="App">
       <h1>Github Finder</h1>
+      <Outlet />
     </div>  
-  )
+  );
 }
 
 export default App
